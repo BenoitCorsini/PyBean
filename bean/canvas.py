0f84c348309f1405ef572690278ca8f29c87d106
BenoitCorsini <benoitcorsini@gmail.com>
2024-09-03 16:36:34 +0200

    Canvas and Brush split

--- bean/canvas.py
+++ bean/canvas.py
@@ -31,10 +31,7 @@ class Canvas(object):
 
     def reset(self):
         self.start_time = time()
-        self.canvas()
-        if hasattr(self, 'copyright'):
-            self.add_copyright()
-        return self
+        return self.canvas()
 
     def canvas(self):
         self.fig = figure.Figure(figsize=self.figsize, dpi=self.dpi)
@@ -45,50 +42,8 @@ class Canvas(object):
         self.ax.set_axis_off()
         return self
 
-    def add_copyright(self):
-        x = self.xmin + (self.xmax - self.xmin)*self.copyright.get('margin', 0)*self.figsize[1]/self.figsize[0]
-        y = self.ymin + (self.ymax - self.ymin)*self.copyright.get('margin', 0)
-        height = (self.ymax - self.ymin)*self.copyright.get('ratio', 1)
-        prop = FontProperties(fname=osp.join(
-            osp.dirname(osp.realpath(__file__)),
-            self.copyright.get('fname', '@BC.otf')
-        ))
-        path = self.path_from_string(
-            s=self.copyright.get('text', 'Benoit Corsini'),
-            x=x,
-            y=y,
-            height=height,
-            prop=prop,
-            anchor='south west',
-        )
-        self.ax.add_patch(patches.PathPatch(
-            path=path,
-            lw=0,
-            color=self.copyright.get('fc', 'black'),
-            **self.copyright.get('params', {})
-        ))
-        self.ax.add_patch(patches.PathPatch(
-            path=path,
-            lw=self.copyright.get('lw', 0),
-            color=self.copyright.get('ec', 'black'),
-            fill=False,
-            **self.copyright.get('params', {})
-        ))
-
-    # https://www.rapidtables.com/code/text/unicode-characters.html
-    def path_from_string(self, s, x=0, y=0, height=1, prop=None, anchor=None):
-        path = TextPath((0, 0), s, prop=prop)
-        bbox = path.get_extents()
-        transform = Affine2D()
-        transform.translate(*(-(bbox.size/2 + bbox.p0)))
-        transform.translate(*self.shift_from_anchor(bbox=bbox, anchor=anchor))
-        transform.scale(height/bbox.size[1])
-        transform.scale(self.figsize[1]/self.figsize[0]*(self.xmax - self.xmin)/(self.ymax - self.ymin), 1)
-        transform.translate(x, y)
-        return path.transformed(transform)
-
     def __str__(self):
-        s = 'PyBean Canvas'
+        s = f'PyBean {self.__class__.__name__}'
         s += f' (figsize={self.figsize},'
         s += f' dpi={self.dpi})'
         if hasattr(self, 'copyright'):
@@ -103,38 +58,19 @@ class Canvas(object):
     def get_kwargs(self):
         return vars(self.parser.parse_args())
 
-    @staticmethod
-    def shift_from_anchor(bbox, anchor=None):
-        if anchor is None:
-            return np.array([0, 0])
-        elif anchor == 'north':
-            return np.array([0, - bbox.size[1]/2])
-        elif anchor == 'south':
-            return np.array([0, bbox.size[1]/2])
-        elif anchor == 'east':
-            return np.array([- bbox.size[0]/2, 0])
-        elif anchor == 'west':
-            return np.array([bbox.size[0]/2, 0])
-        elif ' ' in anchor:
-            anchor = anchor.strip()
-            return np.sum([Canvas.shift_from_anchor(bbox, anc) for anc in anchor.split(' ')], axis=0)
-        else:
-            return np.array([0, 0])
-
     def save(self, name='image', image_dir=None, transparent=False):
         if image_dir is None:
-            image_dir = self.image_dir
+            image_dir = '.'
         if not osp.exists(image_dir):
             os.makedirs(image_dir)
-        self.fig.savefig(osp.join(image_dir, name + '.png'), transparent=transparent)
-
-    @staticmethod
-    def rgb_to_hex(r, g, b):
-        return f'#{r:02x}{g:02x}{b:02x}'
+        self.fig.savefig(
+            osp.join(image_dir, name + '.png'),
+            transparent=transparent
+        )
 
     @staticmethod
     def get_cmap(colour_list):
-        return LinearSegmentedColormap.from_list(f'cmap of Benoit', colour_list)
+        return LinearSegmentedColormap.from_list(f'pybean', colour_list)
 
     @staticmethod
     def get_greyscale(start_with_white=True):
@@ -142,7 +78,7 @@ class Canvas(object):
             colour_list = ['white', 'black']
         else:
             colour_list = ['black', 'white']
-        return LinearSegmentedColormap.from_list(f'greyscale of Benoit', colour_list)
+        return LinearSegmentedColormap.from_list(f'pybean', colour_list)
 
     @staticmethod
     def get_cmap_from_colour(colour='grey', start_with='white', end_with='black'):
@@ -155,7 +91,7 @@ class Canvas(object):
             colour_list = [start_with, colour]
         else:
             colour_list = [start_with, colour, end_with]
-        return Figure.get_cmap(colour_list)
+        return LinearSegmentedColormap.from_list(f'pybean', colour_list)
 
     @staticmethod
     def time_to_string(time):
