cf71c3a6bdd624d970bb6cb3e5c60d7919c8a8e7
BenoitCorsini <benoitcorsini@gmail.com>
2024-09-04 13:09:06 +0200

    Moving to volume

--- bean/volume.py
+++ bean/volume.py
@@ -1,54 +1,18 @@
-import matplotlib.patches as patches
-from matplotlib.transforms import Affine2D
-from matplotlib.textpath import TextPath
-
-from .canvas import Canvas
+from .shape import Shape
 
 
-class Volume(Canvas):
+class Volume(Shape):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+            self: Self,
+            **kwargs,
+        ) -> None:
+        # initiate class
+        super().__init__(**kwargs)
+        self._new_volume()
 
-    def save_image(self, *args, **kwargs):
-        self.save(*args, **kwargs)
-        print('Time to plot image: ' + self.time())
-
-    def plot_shape(self, shape_name, **kwargs):
-        patch = getattr(patches, shape_name)(**kwargs)
-        self.ax.add_patch(patch)
-        return patch
-
-    def grid(self, x_blocks=1, y_blocks=None, xmin=None, xmax=None, ymin=None, ymax=None, **kwargs):
-        if y_blocks is None:
-            y_blocks = x_blocks
-        if xmin is None:
-            xmin = self.xmin
-        if xmax is None:
-            xmax = self.xmax
-        if ymin is None:
-            ymin = self.ymin
-        if ymax is None:
-            ymax = self.ymax
-        columns = []
-        rows = []
-        kwargs['capstyle'] = 'butt'
-        delta_x = (xmax - xmin)/x_blocks
-        for x in range(0, x_blocks + 1):
-            pos = xmin + x*delta_x
-            column_patch = self.plot_shape(
-                shape_name='Polygon',
-                xy=[[pos, ymin], [pos, ymax]],
-                **kwargs
-            )
-            columns.append(column_patch)
-        delta_y = (ymax - ymin)/y_blocks
-        for y in range(0, y_blocks + 1):
-            pos = ymin + y*delta_y
-            row_patch = self.plot_shape(
-                shape_name='Polygon',
-                xy=[[xmin, pos], [xmax, pos]],
-                **kwargs
-            )
-            rows.append(row_patch)
-        return columns, rows
+    def _new_shape(
+            self: Self,
+        ) -> Self:
+        # new volume instance
+        return self
--- example.py
+++ example.py
@@ -2,8 +2,11 @@ import bean
 
 
 if __name__ == '__main__':
-	B = bean.Canvas()
-	B.main()
-	print()
-	B = bean.Shape()
+	# B = bean.Canvas()
+	# B.main()
+	# print()
+	# B = bean.Shape()
+	# B.main()
+	# print()
+	B = bean.Volume()
 	B.main()
