2a3e1af5fd4cc3a19b16d10f717c058cafda2335
BenoitCorsini <benoitcorsini@gmail.com>
2024-09-10 19:42:58 +0200

    Sphere created

--- bean/volume.py
+++ bean/volume.py
@@ -45,26 +45,62 @@ class Volume(Shape):
             pos: (float, float, float),
         ) -> (float, float):
         # transforms a 3D position into a 2D coordinate
-        if pos is None:
-            raise ValueError('None position cannot be projected')
         side, depth, altitude = pos
         return side, depth
+
+    def _create_sphere(
+            self: Self,
+            name: str,
+        ) -> dict:
+        # creates the voluem dictionary of a sphere
+        volume = {}
+        key = f'{name}_main'
+        volume['main'] = key
+        self.add_shape(
+            shape_name='Circle',
+            key=key,
+            xy=(0, 0),
+            lw=0,
+            zorder=0,
+        )
+        volume['side'] = []
+        for side_index, _ in enumerate(self.sphere_side):
+            key = f'{name}_side{side_index}'
+            volume['side'].append(key)
+            self.add_raw_path(
+                key=key,
+                vertices=[(0, 0)],
+                lw=0,
+                zorder=0,
+            )
+        key = f'{name}_shade'
+        volume['shade'] = key
+        self.add_shape(
+            shape_name='Circle',
+            key=key,
+            xy=(0, 0),
+            lw=0,
+            zorder=-1,
+        )
+        return volume
 
     def new_sphere(
             self: Self,
             pos: (float, float, float) = None,
-            xy: (float, float) = None,
+            xy: (float, float) = (0, 0),
+            radius: float = 1,
             key: Any = None,
         ) -> None:
         # create the basis for a new sphere
-        if pos is not None:
-            xy = self._pos_to_xy(pos)
         key, available = self.key_checker(key=key, category='volume')
         if available:
             volume = {
-                'main' : f'{key}_main',
-                'shade' : f'{key}_shade',
+                'type' : 'sphere',
+                'pos' : pos,
+                'xy' : xy,
+                'radius' : radius,
             }
+            volume = self._create_sphere(name=key)
             self._volumes[key] = volume
         else:
             volume = self._volumes[key]
@@ -78,7 +114,7 @@ class Volume(Shape):
         print(self._get_classes())
         print(self._get_new_methods())
         print(self.get_kwargs())
-        # self.new_sphere()
+        self.new_sphere()
         # self.new_sphere()
         # self.new_sphere('other')
         self.save()
