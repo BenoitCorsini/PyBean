cf9ca83978ff8d21a70a66eb30cd85471832a9e9
BenoitCorsini <benoitcorsini@gmail.com>
2024-11-07 15:58:32 +0100

    Motion cleaned and early stops added

--- bean/motion.py
+++ bean/motion.py
@@ -48,18 +48,15 @@ class Motion(Volume):
             motion: dict,
         ) -> None:
         # adds a motion to the current dictionary
-        motion['motion_index'] = self._motion_index
         self._motions[self._motion_index] = motion
         self._motion_index += 1
 
     def _frames_to_video(
             self: Self,
             name: str = 'video',
-            video_dir: str = None,
+            video_dir: str = '.',
         ) -> str:
         # transforms the frames into a video
-        if video_dir is None:
-            video_dir = '.'
         if not osp.exists(video_dir):
             os.makedirs(video_dir)
         video_file = osp.join(video_dir, name + '.mp4')
@@ -102,19 +99,12 @@ class Motion(Volume):
             time: float = None,
             key: Any = None,
             time_dict: dict = 'times',
-            plot_info: bool = False,
         ):
         # returns the number of frames corresponding to params
-        info = f'Waiting {nfs} frame'
-        info += '' if nfs <= 1 else 's'
         if key is not None:
             nfs = self._key_to_number_of_frames(key, time_dict)
-            info = str(key)
         elif time is not None:
             nfs = self._time_to_number_of_frames(time)
-            info = f'Waiting {time}s'
-        if self.draft and plot_info:
-            self.show_info(info)
         return nfs
 
     def _get_number_of_frames(
@@ -160,7 +150,7 @@ class Motion(Volume):
                 self._volumes[volume_key][freq_shift_key] = freq_shift
             else:
                 freq_shift = kwargs.pop(freq_shift_key)
-            side, depth, altitude = kwargs[pos_param]
+            side, depth, altitude = self._normalize_pos(kwargs[pos_param])
             extra_altitude = (1 - np.cos(
                 2*np.pi*(
                     freq_shift + self._frame_index/self.fps
@@ -180,6 +170,7 @@ class Motion(Volume):
             avoid: Any = [],
             duration: Any = 1,
             delay: Any = 0,
+            early_stop: Any = None,
             **kwargs,
         ) -> Self:
         # change the radius of the volume according to the given parameters
@@ -190,8 +181,18 @@ class Motion(Volume):
                 'volume' : volume,
                 'duration' : self._get_number_of_frames(duration),
                 'method' : f'_apply_{method}_{volume_name}',
-                'step' : 1 - self._get_number_of_frames(delay),
+                'step' : -self._get_number_of_frames(delay),
             }
+            if early_stop is not None:
+                if isinstance(early_stop, list):
+                    motion['early_stops'] = sorted([
+                        self._get_number_of_frames(stop)
+                        for stop in early_stop
+                    ])
+                else:
+                    motion['early_stops'] = [
+                        self._get_number_of_frames(early_stop)
+                    ]
             motion.update(kwargs)
             getattr(self, f'_add_{method}_{volume_name}')(**motion)
         return self
@@ -202,16 +203,20 @@ class Motion(Volume):
             motion_index: int,
             step: int,
             duration: int,
+            early_stops: list[int] = None,
             **kwargs,
         ) -> bool:
         # runs a specific motion and returns whether it is finished or not
         finished = False
-        if step >= duration:
-            step = duration
+        if step >= duration - 1:
+            step = duration - 1
             finished = True
         self._motions[motion_index]['step'] = step + 1
-        if step >= 1:
-            getattr(self, method)(step=step, duration=duration, **kwargs)
+        if step >= 0:
+            getattr(self, method)(step=step + 1, duration=duration, **kwargs)
+        if early_stops is not None and not finished:
+            if step >= early_stops[0] - 1:
+                raise StopIteration
         return finished
 
     def _add_change_radius_sphere(
@@ -253,15 +258,16 @@ class Motion(Volume):
         radius = start_radius + step*delta_radius
         pos = self._volumes[volume].get('pos', None)
         if pos is not None and centred:
-            if not step:
-                if start_radius != self._volumes[volume]['radius']:
-                    delta_radius = min(0, start_radius -  end_radius)
-                else:
-                    delta_radius = 0
+            if step:
+                delta_altitude = delta_radius
+            elif start_radius != self._volumes[volume]['radius']:
+                delta_altitude = min(0, start_radius -  end_radius)
+            else:
+                delta_altitude = 0
             self._volumes[volume]['pos'] = (
                 pos[0],
                 pos[1],
-                pos[2] - delta_radius,
+                pos[2] - delta_altitude,
             )
         self._volumes[volume]['radius'] = radius
 
@@ -299,6 +305,57 @@ class Motion(Volume):
         # changes the radius of sphere
         alpha = start_alpha + (end_alpha - start_alpha)*step/duration
         self._volumes[volume]['alpha'] = alpha
+
+    # def _add_move_sphere(
+    #         self: Self,
+    #         volume: Any,
+    #         start_at: float = 1,
+    #         end_at: float = 1,
+    #         centred: bool = True,
+    #         **kwargs,
+    #     ) -> None:
+    #     # changes the radius of sphere
+    #     if start_with == end_with:
+    #         return None
+    #     motion = {
+    #         'volume' : volume,
+    #         'centred' : centred,
+    #     }
+    #     motion.update(kwargs)
+    #     radius = self._volumes[volume].get('radius', 1)
+    #     for timing in ['start', 'end']:
+    #         timing_with = locals()[f'{timing}_with']
+    #         if timing_with >= 0:
+    #             motion[f'{timing}_radius'] = radius*timing_with
+    #         else:
+    #             motion[f'{timing}_radius'] = abs(timing_with)
+    #     self._add_motion(motion)
+
+    # def _apply_move_sphere(
+    #         self: Self,
+    #         volume: Any,
+    #         step: int,
+    #         duration: Any,
+    #         start_radius: float,
+    #         end_radius: float,
+    #         centred: bool,
+    #     ) -> bool:
+    #     # changes the radius of sphere
+    #     delta_radius = (end_radius - start_radius)/duration
+    #     radius = start_radius + step*delta_radius
+    #     pos = self._volumes[volume].get('pos', None)
+    #     if pos is not None and centred:
+    #         if not step:
+    #             if start_radius != self._volumes[volume]['radius']:
+    #                 delta_radius = min(0, start_radius -  end_radius)
+    #             else:
+    #                 delta_radius = 0
+    #         self._volumes[volume]['pos'] = (
+    #             pos[0],
+    #             pos[1],
+    #             pos[2] - delta_radius,
+    #         )
+    #     self._volumes[volume]['radius'] = radius
 
     '''
     general methods
@@ -347,7 +404,7 @@ class Motion(Volume):
         # waits before next motion
         for _ in range(self._get_number_of_frames(*args, **kwargs)):
             self.new_frame()
-        return Self
+        return self
 
     def video(
             self: Self,
@@ -370,10 +427,19 @@ class Motion(Volume):
             self: Self,
         ) -> Self:
         # runs through the current motions
-        while self._motions:
+        no_early_stop = True
+        while self._motions and no_early_stop:
             finished_motions = []
             for index, motion in self._motions.items():
-                finished = self._run_motion(**motion)
+                try:
+                    finished = self._run_motion(motion_index=index, **motion)
+                except StopIteration:
+                    early_stops = motion['early_stops']
+                    early_stops.pop(0)
+                    if not early_stops:
+                        del motion['early_stops']
+                    finished = False
+                    no_early_stop = False
                 if finished:
                     finished_motions.append(index)
             for index in finished_motions:
--- tests/motion_tests.py
+++ tests/motion_tests.py
@@ -73,40 +73,87 @@ class MotionTests(unittest.TestCase):
     def test_appear_disappear(self):
         self.mt.draft = True
         self.mt.levitation_mode = 'off'
-        self.mt.reset().wait(0.1)
+        self.mt.reset()
+        self.mt.show_info('waiting')
+        self.mt.wait(0.1)
+        self.mt.show_info('blue grows')
         self.mt.new_sphere(
             'blue',
             pos=(0.2, 0.1, 0),
             radius=0.2,
             colour='royalblue',
-        ).grow('blue', duration=0.5, centred=False).run().wait(0.1)
+        ).grow('blue', duration=0.5, centred=False, early_stop=0.1).run()
+        self.mt.show_info('pause')
+        self.mt.wait(0.1)
+        self.mt.show_info('blue and shaded yellow grow, then yellow grows/appears')
         self.mt.new_sphere(
             'yellow',
             pos=(0.6, 0.3, 0),
             radius=0.2,
             colour='gold',
             alpha=0.1,
-        ).grow('yellow', duration=0.5).change_alpha('yellow', start_with=1, end_with=-1, duration=0.1, delay=0.4).run().wait(0.1)
+        ).grow('yellow', duration=0.5).change_alpha('yellow', start_with=1, end_with=-1, duration=0.1, delay=0.4).run()
+        self.mt.show_info('pause')
+        self.mt.wait(0.1)
+        self.mt.show_info('green grows and appears slowly')
         self.mt.new_sphere(
             'green',
             pos=(0.85, 0.15, 0),
             radius=0.2,
             colour='forestgreen',
             alpha=0.5,
-        ).appear('green', duration=1.).grow('green', duration=0.5, centred=False).run().wait(0.1)
+        ).appear('green', duration=1.).grow('green', duration=0.5, centred=False).run()
+        self.mt.show_info('pause')
+        self.mt.wait(0.1)
+        self.mt.show_info('all schrink')
+        self.mt.change_radius(
+            start_with=1,
+            end_with=0.5,
+            duration=0.1,
+        ).run()
+        self.mt.show_info('delay, then all grow, stop, grow, stop, grow')
+        self.mt.change_radius(
+            start_with=1,
+            end_with=2,
+            duration=0.5,
+            delay=0.1,
+            early_stop=[0.1, 0.2],
+        ).run().wait(0.1).run().wait(0.1).run()
+        self.mt.show_info('red appears')
         self.mt.new_sphere(
             'red',
             pos=(0.3, 0.5, 0.1),
             radius=0.1,
             colour='crimson',
-        ).grow('red', duration=0.5).run().wait(0.1)
+        ).grow('red', duration=0.5).run()
+        self.mt.show_info('pause')
+        self.mt.wait(0.1)
+        self.mt.show_info('green disappears')
         self.mt.disappear('green', duration=0.5).run()
+        self.mt.show_info('green appears')
         self.mt.appear('green', use_current_alpha=False, duration=0.5).run()
-        self.mt.schrink(avoid='blue', duration=0.5).run().wait(0.1)
+        self.mt.show_info('all but blue schrink')
+        self.mt.schrink(avoid='blue', duration=0.5).run()
+        self.mt.show_info('pause')
+        self.mt.wait(0.1)
         self.mt.update(alpha=1)
+        self.mt.show_info('all but blue grow')
         self.mt.change_radius(start_with=1, end_with=-0.2, duration=0.5).run()
-        self.mt.schrink(duration=0.5).disappear(duration=0.5).run().wait(0.1)
-        self.mt.video()
+        self.mt.show_info('all schrink and disappear')
+        self.mt.schrink(duration=0.5).disappear(duration=0.5).run()
+        self.mt.show_info('end')
+        self.mt.wait(0.1)
+        self.mt.video('video_appear')
+
+    # def test_movement(self):
+    #     # self.mt.draft = True
+    #     # self.mt.levitation_mode = 'off'
+    #     self.mt.reset().wait(0.1)
+    #     self.mt.new_sphere(
+    #         pos=(0.5, 0, 0),
+    #         radius=0.1,
+    #     ).grow(duration=0.5, centred=False).run().wait(0.1)
+    #     self.mt.video()
 
 
 if __name__ == '__main__':
