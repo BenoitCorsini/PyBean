6696d9242a3f248513320a0230828b0139212a30
BenoitCorsini <benoitcorsini@gmail.com>
2025-02-07 13:39:50 +0800

    Testing shadows

--- run.py
+++ run.py
@@ -1,22 +1,19 @@
-import pybean
+from bean import Motion
+
 
 if __name__ == '__main__':
-	bean = pybean.Motion()
-	bean.levitation_mode = 'random'
-	bean.draft = False
-	bean.scale = 0.5
-
-	bean.new_sphere('blue', pos=(0.2, 0.1, 0), radius=0.2, colour='royalblue')
-	bean.update()
-
-	bean.new_sphere('sphere', pos=(0.6, 0.3, 0), radius=0.2, colour='gold')
-	bean.update()
-
-	bean.new_sphere('G', pos=(0.85, 0.15, 0), radius=0.2, colour='forestgreen')
-	bean.appear('G', duration=1.)
-
-	bean.new_sphere('tiny', pos=(0.3, 0.5, 0.1), radius=0.1, colour='crimson')
-	bean.appear('tiny', duration=0.2)
-
-	bean.disappear(avoid='blue', duration=1.)
-	bean.disappear('blue', duration=0.2)
+    bean = Motion()
+    bean.levitation_mode = 'off'
+    for horizon_angle in [0, 10, 20, 30, 40, 44]:
+        for shade_angle in [0, 45, 90, 135, 180, 225, 270, 315]:
+            for altitude_to_shade in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 1.0]:
+                bean.horizon_angle = horizon_angle
+                bean.shade_angle = shade_angle
+                bean.altitude_to_shade = altitude_to_shade
+                bean.reset()
+                bean.new_sphere(radius=0.05, pos=(0, 0.5))
+                bean.new_sphere(radius=0.05, pos=(1, 0.5))
+                bean.new_sphere(radius=0.05, pos=(0.5, 0.2))
+                bean.new_sphere(radius=0.05, pos=(0.5, 0.2, 0.5))
+                bean.update()
+                bean.save(f'{horizon_angle}_{shade_angle}_{altitude_to_shade}', image_dir='shade_test')
