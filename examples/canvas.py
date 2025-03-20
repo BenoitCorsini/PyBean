536f3880f685b46c4fe24cf0b58db72ac9799f56
BenoitCorsini <benoitcorsini@gmail.com>
2025-03-20 17:06:11 +0800

    Canvas example added

---
+++ examples/canvas.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from bean import Canvas
+
+CV = Canvas(seed=0)
+CV.add_arg('--cscale_colour')
+CV.set_args(True)
+
+cmap = Canvas.get_cscale(colour=CV.cscale_colour)
+step = 0.02
+linewidth = 10
+for pos in np.arange(start=step, stop=0.5, step=step):
+	random_colour = cmap(np.random.rand())
+	CV.ax.plot(
+		[pos, 1 - pos, 1 - pos, pos, pos],
+		[pos, pos, 1 - pos, 1 - pos, pos],
+		color=random_colour,
+		lw=linewidth,
+	)
+CV.save(
+	name=f'canvas {CV.cscale_colour}',
+	image_dir='examples',
+)
